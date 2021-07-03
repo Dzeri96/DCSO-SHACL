c827d65b8b1687c743aa88705d6656e993164438
Filip Darmanovic <e1527089@student.tuwien.ac.at>
2021-07-03 14:29:35 +0200

    Cleaned up huge in statements

--- validate.sh
+++ validate.sh
@@ -3,9 +3,14 @@
 FILES="./instances/*"
 for f in $FILES
 do
-  echo "Processing $f file..."
-  # take action on each file. $f store current file name
-  #cat "$f"
   BASENAME="$(basename -- $f)"
-  ../shacl-1.3.2/bin/shaclvalidate.sh -datafile ./instances/gdp_sunshine-maDMP.ttl -shapesfile maDMP-1-1.SHACL-schema.ttl > "./validation_results/$BASENAME-result.ttl"
+  echo "Validating $f ..."
+
+  ../shacl-1.3.2/bin/shaclvalidate.sh -datafile instances/gdp_sunshine-maDMP.ttl -shapesfile maDMP-1-1.SHACL-schema.ttl > "./validation_results/$BASENAME-result.ttl"
+
+  if [ $? -eq 0 ]; then
+    echo "  Validation passed!"
+  else
+    echo "  Validation failed! Check the output file for more info."
+  fi
 done
